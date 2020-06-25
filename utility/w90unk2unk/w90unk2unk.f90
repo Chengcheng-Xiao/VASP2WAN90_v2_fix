81c56e77fbcd5def894f7074e56959c735d3545a
Chengcheng_Xiao <iconxicon@me.com>
2020-06-25 18:29:58 +0100

    fixed w90unk2unk.

--- utility/w90unk2unk/w90unk2unk.f90
+++ utility/w90unk2unk/w90unk2unk.f90
@@ -6,7 +6,7 @@ implicit none
    character*20 :: wfnname_in,wfnname_out
 
 201   format ('UNK',i5.5,'.','NC')
-202   format ('UNK',i5.5,'.','NC_1')
+202   format ('UNK',i5.5,'.','NC.formatted')
 
    ! read header
    open(3, file='UNK00001.NC', FORM='UNFORMATTED')
@@ -33,12 +33,12 @@ implicit none
 
      ! get new file name (here, I choose to overwrite.)
      write(wfnname_out,201) I
-     open(2, file=wfnname_out,FORM='UNFORMATTED')
-     write(2) ngx, ngy, ngz, nk, nbnd
+     open(2, file=wfnname_out,FORM='FORMATTED')
+     write(2,*) ngx, ngy, ngz, nk, nbnd
      do ibnd=1,nbnd
        !write(*,*) ibnd
-       write(2) (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
-       write(2) (-r_wvfn_nc(nx, ibnd, 2), nx=1, ngx*ngy*ngz)
+       write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
+       write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 2), nx=1, ngx*ngy*ngz)
      enddo
      close(2)
      deallocate (r_wvfn_nc)
