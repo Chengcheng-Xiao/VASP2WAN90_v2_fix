901698af6c7a9d66b44724e0d1afb942a8559eea
Chengcheng-Xiao <iconxicon@me.com>
2021-05-20 11:50:28 +0100

    fix w90unk2unk.f90

--- utility/w90unk2unk/README.md
+++ utility/w90unk2unk/README.md
@@ -10,5 +10,14 @@ gfortran -o w90unk2unk w90unk2unk.f90
 ```
 
 ## Usage
-By default, this only works with `UNKxxxxx.NC` file.
-if you want to change this behavior, simply change all `NC` inside the source code to `1` or `2` and recompile.
+for collinear spin/spin degenerate systems:
+```
+w90unk2unk 0 UNKxxxxx.1
+```
+will generate a new formatted file `UNKxxxxx.1.formatted`
+
+for collinear spin/spin degenerate systems:
+```
+w90unk2unk 1 UNKxxxxx.NC
+```
+will generate a new formatted file `UNKxxxxx.NC.formatted`
--- utility/w90unk2unk/w90unk2unk.f90
+++ utility/w90unk2unk/w90unk2unk.f90
@@ -1,48 +1,76 @@
 program w90unk2unk
 implicit none
 
-   integer :: ngx, ngy, ngz, nk, nbnd, I, ibnd,nx
+   integer :: ngx, ngy, ngz, nk, nbnd, I, ibnd, nx, soc
    complex(kind=8), allocatable :: r_wvfn_nc(:, :, :)
-   character*20 :: wfnname_in,wfnname_out
+   !character*20 :: wfnname_in,wfnname_out
+   character(len=32) :: arg
 
-201   format ('UNK',i5.5,'.','NC')
-202   format ('UNK',i5.5,'.','NC.formatted')
+   ! 201   format ('UNK',i5.5,'.','NC')
+   ! 202   format ('UNK',i5.5,'.','NC.formatted')
 
-   ! read header
-   open(3, file='UNK00001.NC', FORM='UNFORMATTED')
-   read (1) ngx, ngy, ngz, nk, nbnd
-   close(3)
+   I = 0
+   do
+     call get_command_argument(I, arg)
+     if (LEN_TRIM(arg) == 0) exit
+     if (I == 1) then
+       read(arg, *) soc
+       if (soc == 1) then
+	 write (*,*) "Non-collinear spin file(s)."
+       else if (soc == 0) then
+         continue
+       else
+         write (*,*) "please enter file type: 0-collinear | 1-noncollinear"
+	 exit
+       endif
+     endif
+     if (I > 1) then
+       write (*,*) "Converting File: "//TRIM(arg)
 
-   ! open file
-   do I=1,nk
-     write(wfnname_in,201) I
-     open(1, file=wfnname_in, FORM='UNFORMATTED')
+       ! open file
+       open(1, file=TRIM(arg), FORM='UNFORMATTED')
 
-     read (1) ngx, ngy, ngz, nk, nbnd
+       read (1) ngx, ngy, ngz, nk, nbnd
+       ! write (*,*) ngx*ngy*ngz*nbnd
 
-     ! allocate array to store data
-     allocate (r_wvfn_nc(ngx*ngy*ngz, nbnd, 2))
+       ! allocate array to store data
+       if (soc) then
+         allocate (r_wvfn_nc(ngx*ngy*ngz, nbnd, 2))
+       else
+	 allocate (r_wvfn_nc(ngx*ngy*ngz, nbnd, 1))
+       endif
 
-     ! read-in actual data
-     do ibnd=1,nbnd
-       !write(*,*) ibnd
-       read(1) (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
-       read(1) (r_wvfn_nc(nx, ibnd, 2), nx=1, ngx*ngy*ngz)
-     enddo
-     close(1)
+       ! read-in actual data
+       do ibnd=1,nbnd
+	 if (soc == 1) then
+           read(1) (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
+           read(1) (r_wvfn_nc(nx, ibnd, 2), nx=1, ngx*ngy*ngz)
+	 else
+           read(1) (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
+	 endif
+       enddo
+       
+       close(1)
 
-     ! get new file name (here, I choose to overwrite.)
-     write(wfnname_out,201) I
-     open(2, file=wfnname_out,FORM='FORMATTED')
-     write(2,*) ngx, ngy, ngz, nk, nbnd
-     do ibnd=1,nbnd
-       !write(*,*) ibnd
-       write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
-       write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 2), nx=1, ngx*ngy*ngz)
-     enddo
-     close(2)
-     deallocate (r_wvfn_nc)
-     WRITE(*,*) "kpoint #", I, " done."
-     ENDDO
+       ! get new file name (here, I choose to overwrite.)
+       open(2, file=TRIM(arg)//'.formated',FORM='FORMATTED')
+       
+       write(2,*) ngx, ngy, ngz, nk, nbnd
+       do ibnd=1,nbnd
+         !write(*,*) ibnd
+	 if (soc == 1) then
+           write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
+           write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 2), nx=1, ngx*ngy*ngz)
+         else
+           write(2,'(2ES20.10)') (r_wvfn_nc(nx, ibnd, 1), nx=1, ngx*ngy*ngz)
+	 endif
+       enddo
+       
+       close(2)
+       
+       deallocate (r_wvfn_nc)
+     endif
+     I = I+1  
+   enddo
 
 end program w90unk2unk
